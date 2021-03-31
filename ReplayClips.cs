eb7961a6257f1b11a921814f52ba1f1cef1103a9
Anton Franzluebbers <anton@uga.edu>
2021-03-31 04:25:00 -0400

    add echoreplay clips

---
+++ ReplayClips.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Spark
+{
+	public class ReplayClips
+	{
+		private const int DelayAfter = 5;
+
+		public ReplayClips()
+		{
+			Program.PlayspaceAbuse += (frame, team, player, offset) =>
+			{
+				Task.Run(() =>
+				{
+					Task.Delay(DelayAfter * 1000);
+					Program.SaveReplayClip($"{player.name}_abuse");
+				});
+			};
+
+			Program.Goal += (frame, goalData) =>
+			{
+				Task.Run(() =>
+				{
+					Task.Delay(DelayAfter * 1000);
+					Program.SaveReplayClip($"{frame.last_score.person_scored}_goal");
+				});
+			};
+		}
+	}
+}
