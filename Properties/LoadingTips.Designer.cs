57f8940ef7952cde575bc220aee5f1ea79a9f23e
NtsFranz <ntsluebbers@gmail.com>
2022-07-14 15:39:03 -0400

    moving resource files around

--- Properties/LoadingTips.Designer.cs
+++ Properties/LoadingTips.Designer.cs
@@ -19,7 +19,7 @@ namespace Spark.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LoadingTips {
@@ -39,7 +39,7 @@ namespace Spark.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Spark.Properties.LoadingTips.LoadingTips", typeof(LoadingTips).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Properties.LoadingTips", typeof(LoadingTips).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
