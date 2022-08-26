d44d54d2ea0c877c904aea6fb295ce0a2f145273
NtsFranz <ntsluebbers@gmail.com>
2022-08-25 21:16:20 -0400

    WiX installer working again

---
+++ Controllers/Notifications.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Spark
+{
+	public class Notifications
+	{
+		public static void ShowNotification()
+		{
+			new ToastContentBuilder()
+				.AddText("Something copied to clipboard")
+				.Show();
+		}
+	}
+}
